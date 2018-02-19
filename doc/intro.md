298354feddd92b8849befb65eab228040f9d3e45
Saul Johnson <saul.a.johnson@gmail.com>
2018-02-19 18:30:06 +0000

    Add project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to matcher-starter
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/matcher_starter/core_test.clj
@@ -0,0 +1,7 @@
+(ns matcher-starter.core-test
+  (:require [clojure.test :refer :all]
+            [matcher-starter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
