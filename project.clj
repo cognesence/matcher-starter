7a9b03905b2a291d7abf46975b4e4c5bf9319f96
REDSHAW <V8196566@live.tees.ac.uk>
2020-02-26 12:07:10 +0000

    Added 'main' annotation to project.clj that imports matcher-starter.core namespace automatically.

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojars.cognesence/breadth-search "0.9.0"]
                  [org.clojars.cognesence/matcher "1.0.1"]
-                 [org.clojars.cognesence/ops-search "1.0.1"]])
+                 [org.clojars.cognesence/ops-search "1.0.1"]]
+  :main matcher-starter.core)
